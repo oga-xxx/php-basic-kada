f06d4a055347e0be7ce8481502e37335a42a0946
oga-xxx <cacaomame.11@gmail.com>
2023-08-15 22:26:03 +0900

    kadai_015を修正

--- kadai_015/class.php
+++ kadai_015/class.php
@@ -16,16 +16,15 @@
         $this->price = $price;
       }
 
-      public function set_price(int $price) {
-        $this->price = $price;
-      }
-
       public function show_price() {
         echo $this->price . '<br>';
       }
     }
 
     $food = new Food('poteto', 250);
+
+    print_r($food);
+      echo '<br>';
 
     class Animal {
       private $name;
@@ -38,10 +37,6 @@
         $this->weight = $weight;
       }
 
-      public function set_height(int $height) {
-        $this->height = $height;
-      }
-
       public function show_height() {
         echo $this->height . '<br>';
       }
@@ -49,12 +44,12 @@
 
     $animal = new Animal('dog', 60, 5000);
 
-    print_r($food);
-    echo '<br>';
     print_r($animal);
     echo '<br>';
-    $food->show_price(250);
-    $animal->show_height(60);
+
+
+    $food->show_price();
+    $animal->show_height();
 
   ?>
 </body>
